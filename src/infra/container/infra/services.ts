3156c9e2aa51f7a4e5609e7fb000e07164aab43d
Arthur Ribeiro <artrsousa1@gmail.com>
2025-10-27 01:11:46 -0300

    feat: add new services to DI container

--- src/infra/container/infra/services.ts
+++ src/infra/container/infra/services.ts
@@ -1,9 +1,12 @@
+import { S3StorageService, s3Client } from '@/infra/services/storage/s3';
 import { RepositoriesDI } from './repositories';
-import { HashingBcryptService } from '@/infra/services';
+import { HashingBcryptService, JWTServiceImpl } from '@/infra/services';
 
 export function configureServices(container: RepositoriesDI) {
   return container
-    .add('HashingService', () => new HashingBcryptService());
+    .add('HashingService', () => new HashingBcryptService())
+    .add('JWTService', () => new JWTServiceImpl())
+    .add('StorageService', () => new S3StorageService(s3Client));
 }
 
 export type ServicesDI = ReturnType<typeof configureServices>;
