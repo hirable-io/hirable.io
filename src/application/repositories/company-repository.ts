f2b746e761c7b49fd9a5225a86ea8dfcf93bef05
Arthur Ribeiro <artrsousa1@gmail.com>
2025-10-27 01:13:45 -0300

    feat: enhance repositories interfaces

--- src/application/repositories/company-repository.ts
+++ src/application/repositories/company-repository.ts
@@ -5,8 +5,19 @@ export namespace CompanyRepository {
     export type Input = Company;
     export type Output = Company;
   }
+
+  export namespace FindBy {
+    export type Input = Partial<{
+      document: string;
+      userId: string;
+      id: string;
+    }>
+
+    export type Output = Company | null;
+  }
 }
 
 export interface CompanyRepository {
   create(input: CompanyRepository.Create.Input): Promise<CompanyRepository.Create.Output>;
+  findBy(input: CompanyRepository.FindBy.Input): Promise<CompanyRepository.FindBy.Output>;
 }
