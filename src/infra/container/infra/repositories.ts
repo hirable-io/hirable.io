aae909d4f2a9e760c1fa57daa41f015e32399b5f
Arthur Ribeiro <artrsousa1@gmail.com>
2025-10-27 01:11:28 -0300

    feat: add VacancyRepository to DI container for vacancy management

--- src/infra/container/infra/repositories.ts
+++ src/infra/container/infra/repositories.ts
@@ -3,7 +3,8 @@ import { dataSource } from '@/infra/orm/typeorm/datasource';
 import {
   UserTypeOrmRepository,
   CandidateTypeOrmRepository,
-  CompanyTypeOrmRepository
+  CompanyTypeOrmRepository,
+  VacancyTypeOrmRepository
 } from '@/infra/orm/typeorm/repositories';
 
 export function configureRepositories() {
@@ -12,7 +13,8 @@ export function configureRepositories() {
     .add('DatasourceManager', () => dataSource.manager)
     .add('UserRepository', () => new UserTypeOrmRepository(dataSource))
     .add('CandidateRepository', () => new CandidateTypeOrmRepository(dataSource))
-    .add('CompanyRepository', () => new CompanyTypeOrmRepository(dataSource));
+    .add('CompanyRepository', () => new CompanyTypeOrmRepository(dataSource))
+    .add('VacancyRepository', () => new VacancyTypeOrmRepository(dataSource));
 }
 
 export type RepositoriesDI = ReturnType<typeof configureRepositories>;
